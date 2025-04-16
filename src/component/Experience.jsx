76215bef745788e0ef553bdd82345f23a641ff51
john-paul360 <360stpaulj@gmail.com>
2025-04-16 15:42:49 +0100

    added some content made some changes

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Experience = () => {
   return (
-    <div className="layout h-[456px] my-5">
+    <div className="layout">
       <div className="w-[396px] h-[120px]">
         <h1 className="text-[#1d1d1d] font-bold text-[38px]">
           Experience Luxury without compromises
         </h1>
       </div>
-      <div className="h-[298px] flex items-center justify-between">
+      <div className="flex flex-col md:flex-row items-center justify-between">
         <div className="border-[1px] rounded-[14px] border-[#f1f1f1] p-5">
           <h1 className="w-[126px] h-[38px] text-[28px] text-[#000000]">
             Confort
@@ -40,25 +40,25 @@ const Experience = () => {
           </p>
         </div>
       </div>
-      <div className="h-[97px] flex flex-row items-center justify-between">
+      <div className="flex flex-col md:flex-row items-center mt-12 md:justify-between">
         <div className="w-[355px] h-[32px]">
           <p className="font-[300] text-[20px]">
             Trusted by Customer Since{" "}
             <span className="font-[500] text-[#000000]">1990</span>{" "}
           </p>
         </div>
-        <div className="flex flex-row w-[542px] gap-[30px]">
-          <div className="w-[152px]">
-            <p className="text-[18px] font-[300]">Trusted Customers</p>
-            <h1 className="font-[500] text-[30px]">2000+</h1>
+        <div className="flex flex-col  md:flex-row gap-2 md:gap-4 med:items-center items-start">
+          <div className="max-w-[130px] w-full h-[97px] flex flex-col gap-2">
+            <p className="text-[15px] font-[300]">Trusted Customers</p>
+            <h1 className="font-[500] text-[25px]">2000+</h1>
           </div>
-          <div className="w-[152px]">
-            <p className="text-[18px] font-[300]">Available Cars</p>
-            <h1 className="font-[500] text-[30px]">134+</h1>
+          <div className="max-w-[130px] w-full h-[97px] flex flex-col gap-2">
+            <p className="text-[15px] font-[300]">Available Cars</p>
+            <h1 className="font-[500] text-[25px]">134+</h1>
           </div>
-          <div className="w-[132px]">
-            <p className="text-[18px] font-[300]">Total Reservation</p>
-            <h1 className="font-[500] text-[30px]">134+</h1>
+          <div className="max-w-[130px] w-full h-[97px] flex flex-col gap-2">
+            <p className="text-[15px] font-[300]">Total Reservation</p>
+            <h1 className="font-[500] text-[25px]">134+</h1>
           </div>
         </div>
       </div>
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,27 +1,24 @@
-import React from 'react';
+import React from "react";
 import logo from "../assets/logo.png";
-
-
 
 const links = [
   { title: "Home", to: "#" },
-  { title: "Contact us", to: "#"},
+  { title: "Contact us", to: "#" },
   { title: "Gallery", to: "gallery" },
   { title: "Services", to: "#" },
   { title: "How It Works", to: "#" },
-  { title: "Privacy", to:"#"},
-  { title: "Terms & Service", to: "#"},
+  { title: "Privacy", to: "#" },
+  { title: "Terms & Service", to: "#" },
 ];
-
 
 const Footer = () => {
   return (
-    <div className="layout h-[118.57px] gap-[24px] my-5">
-      <div className='flex items-center justify-between '>
-        <div className='bg-[#000000] w-[85.69px] h-[28.57px]'>
+    <div className="layout my-5">
+      <div className="flex flex-col md:flex-row gap-5 md:justify-between md:h-[28.57px] mb-5 items-center justify-between ">
+        <div className="bg-[#000000] w-[85.69px] h-[28.57px]">
           <img src={logo} alt="trex logo" />
         </div>
-        <div className="h-[27px] text-[16px] font-medium underline  flex gap-6 text-[#1d1d1d]">
+        <div className="md:h-[27px] text-[16px] font-medium underline items-center flex flex-col gap-5 text-[#1d1d1d]">
           {links.map((link, index) => {
             return (
               <a key={index} href={link.to}>
@@ -31,13 +28,19 @@ const Footer = () => {
           })}
         </div>
       </div>
-      <p className='h-[66px] font-sm text-[15px] spacing-0% text-[#2f2f2f] text-center my-5'>
-        This website is owned and operated by Masref Ltd, incorporated in the United Kingdom the information provided is not legally binding and does not constitute an offering, 
-        <br/>endorsement, recommendation or solicitation to enter into any type of finencial transaction in this or in any other jurisdiction in which solicitation or offer would be unlawful
-        <br />under the laws of such jurisdiction
+      <p className="h-[66px] font-sm text-[15px] text-[#2f2f2f]">
+        This website is owned and operated by Masref Ltd, incorporated in the
+        United Kingdom the information provided is not legally binding and does
+        not constitute an offering,
+        <br />
+        endorsement, recommendation or solicitation to enter into any type of
+        finencial transaction in this or in any other jurisdiction in which
+        solicitation or offer would be unlawful
+        <br />
+        under the laws of such jurisdiction
       </p>
     </div>
   );
-}
+};
 
 export default Footer;
